047e87cbe40bd9a516efb5024c41dbc6546978da
Denys Kovtun <denys.kovtun@gmail.com>
2023-08-13 19:58:54 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,18 @@
 
 import { List } from './ContactList.styled';
 import { ContactItem } from 'components/ContactItem/ContactItem';
-import { deleteContact } from 'redux/operations';
+import { deleteContact, fetchContacts } from 'redux/operations';
 import Notiflix from 'notiflix';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
+import { useEffect } from 'react';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(fetchContacts());
+    }, [dispatch]);
 
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
@@ -23,6 +28,7 @@ export const ContactList = () => {
       Notiflix.Notify.warning(
         'No contacts matching your request'
       );
+      return [];
     }
     return filteredContacts;
   };
